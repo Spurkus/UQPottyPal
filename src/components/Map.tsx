b233f4853e4f289f00f69761b17eb84eebf09436
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-09-05 22:12:48 +1000

    Add minZoom, maxZoom, and maxBounds to Map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -14,8 +14,10 @@ const Map = () => {
 
   // Initialize the map
   useEffect(() => {
-    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
     if (!mapContainer.current) return; // Initialize the map only once
+
+    // Set the access token
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN as string;
 
     // Create the map
     map.current = new mapboxgl.Map({
@@ -23,6 +25,14 @@ const Map = () => {
       style: process.env.NEXT_PUBLIC_MAPBOX_STYLE as string,
       center: [INITIAL_COORDINATES.lng, INITIAL_COORDINATES.lat],
       zoom: INITIAL_COORDINATES.zoom,
+      minZoom: 15,
+      maxZoom: 18,
+      maxBounds: [
+        INITIAL_COORDINATES.lng - 0.01,
+        INITIAL_COORDINATES.lat - 0.01,
+        INITIAL_COORDINATES.lng + 0.01,
+        INITIAL_COORDINATES.lat + 0.01,
+      ],
     });
 
     // Add navigation controls
@@ -31,6 +41,8 @@ const Map = () => {
       setLat(+map!.current!.getCenter().lat.toFixed(5));
       setZoom(+map!.current!.getZoom().toFixed(2));
     });
+
+    // Add
   }, [setLng, setLat, setZoom]);
 
   const ZoomButton = () => {
@@ -38,11 +50,11 @@ const Map = () => {
     const zoomOut = () => moveZoomTo(zoom - 1);
 
     return (
-      <div className="absolute right-4 top-4 z-10 flex flex-col space-y-2">
-        <button className="btn btn-circle btn-sm" onClick={zoomIn}>
+      <div className="join join-vertical absolute right-4 top-4 z-10 flex flex-col">
+        <button className="btn join-item no-animation btn-sm px-2" onClick={zoomIn}>
           <span className="text-xl">+</span>
         </button>
-        <button className="btn btn-circle btn-sm" onClick={zoomOut}>
+        <button className="btn join-item no-animation btn-sm px-2" onClick={zoomOut}>
           <span className="text-xl">-</span>
         </button>
       </div>
